8bdea3f1ad1ec8b66051b44d554187553295aaf6
Chris Hennes <chennes@gmail.com>
2021-02-12 10:42:00 -0600

    Add skeleton SARIF class

--- src/SARIF.cpp
+++ src/SARIF.cpp
@@ -20,27 +20,53 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-#include <catch2/catch_test_macros.hpp>
+#include "SARIF.h"
 
-#include "../Cleaner.h"
-#include <QFile>
-#include <QTemporaryFile>
+#include <exception>
 
-bool bytesAreEqual(const QByteArray &a, const QByteArray &b) {
-#if QT_VERSION >= QT_VERSION_CHECK(5, 12, 0)
-	return a.compare(b) == 0;
-#else
-	if (a.size() == b.size()) {
-		for (int byte = 0; byte < a.size(); ++byte) {
-			if (a[byte] != b[byte])
-				return false;
-		}
-		return true;
-	}
-	return false;
-#endif
+SARIF::SARIF(const std::string& file)
+{
+	throw std::exception("Unwritten");
 }
 
-TEST_CASE("Failing test", "[cleaner]") {
-	REQUIRE(false);
+std::vector<std::tuple<std::string, std::string>> SARIF::Rules() const
+{
+	return std::vector<std::tuple<std::string, std::string>>();
+}
+
+std::string SARIF::GetBase() const
+{
+	return std::string();
+}
+
+void SARIF::SetBase(const std::string& newBase)
+{
+}
+
+int SARIF::SuppressRule(const std::string& ruleID)
+{
+	return 0;
+}
+
+void SARIF::UnsuppressRule(const std::string& ruleID)
+{
+}
+
+std::vector<std::string> SARIF::SuppressedRules() const
+{
+	return std::vector<std::string>();
+}
+
+int SARIF::AddLocationFilter(const std::string& regex)
+{
+	return 0;
+}
+
+void SARIF::RemoveLocationFilter(const std::string& regex)
+{
+}
+
+std::vector<std::string> SARIF::LocationFilters() const
+{
+	return std::vector<std::string>();
 }
---
+++ src/SARIF.h
@@ -0,0 +1,92 @@
+// MIT License
+//
+// Copyright(c) 2021 Chris Hennes <chennes@pioneerlibrarysystem.org>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and /or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include <string>
+#include <vector>
+
+class SARIF
+{
+public:
+	
+	/**
+	 * \brief Construct a SARIF object from a SARIF-formatted input file
+	 * \throws std::runtime_exception if the file cannot be loaded
+	 * \param file The full path to the input file
+	 */
+	explicit SARIF(const std::string &file);
+
+	/**
+	 * \brief List the rules present in this SARIF object
+	 * \returns a tuple containing the ID of the rule, and its help text
+	 */
+	std::vector<std::tuple<std::string, std::string>> Rules() const;
+
+	/**
+	 * \brief Get the part of the artifactLocation that all results have in common
+	 */
+	std::string GetBase() const;
+
+	/**
+	 * \brief Modify the artifactLocation in the SARIF results to be "rebased" on a new location
+	 * \param newBase the new location
+	 * \see GetBase()
+	 */
+	void SetBase(const std::string &newBase);
+
+	/**
+	 * \brief When outputting this object, don't include rule \s ruleID
+	 * \param ruleID The ID of the rule to suppress
+	 * \returns The number of results that this filter will remove (independent for each rule)
+	 */
+	int SuppressRule(const std::string &ruleID);
+
+	/**
+	 * \brief Remove suppression of a rule
+	 * \param ruleID The ID of the rule to unsuppress
+	 * \see SuppressRule()
+	 */
+	void UnsuppressRule(const std::string &ruleID);
+
+	/**
+	 * \brief Get a list of the currently-suppressed rules 
+	 */
+	std::vector<std::string> SuppressedRules() const;
+
+	/**
+	 * \brief Suppress the output of results whose artifactLocation matches a regular expression
+	 * \param regex The regular expression to apply to the artifactLocation
+	 * \returns The number of results this filter will remove (independent of any other filter)
+	 */
+	int AddLocationFilter(const std::string &regex);
+
+	/**
+	 * \brief Stop suppression of results matching a given regex
+	 * \param regex The regular expression to remove from the suppression list
+	 * \see AddLocationFilter()
+	 */
+	void RemoveLocationFilter(const std::string& regex);
+
+	/**
+	 * \brief Get the list of currently-active location filters 
+	 */
+	std::vector<std::string> LocationFilters() const;
+};
